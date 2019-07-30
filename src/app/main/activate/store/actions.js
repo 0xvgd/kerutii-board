5228989469fe86720719e94611455eb51b4479cc
voyager-gold <eugenemathew93@gmail.com>
2019-07-30 06:07:57 -0700

    use valid auth url

--- src/app/main/activate/store/actions.js
+++ src/app/main/activate/store/actions.js
@@ -3,25 +3,29 @@ import * as Actions from 'app/auth/store/actions';
 
 export function activate(params)
 {
-    const request = axios.post('/api/auth/login', {
-        email: 'info@example.com',
-        password: 'ciao',
-        full_user: true
-    });
+    // const request = axios.post('/api/auth/login', {
+    //     email: 'info@example.com',
+    //     password: 'ciao',
+    //     full_user: true
+    // });
+    
+    const request = axios.get(`/api/rfq/auth/${params}`);
 
     return (dispatch) =>
         new Promise((resolve, reject) => {
             request.then((response) => {
-                response.data.email = 'alison@rocha.com';
-                response.data.name = 'Alison';
-                response.data.lastname = 'Rocha';
+                // response.data.email = 'alison@rocha.com';
+                // response.data.name = 'Alison';
+                // response.data.lastname = 'Rocha';
 
-                localStorage.setItem('auth', JSON.stringify(response.data));
-                axios.defaults.headers.Authorization = `Token ${response.data.token}`;
+                const auth = { ...response.data.auth, ...response.data.user };
 
-                resolve(response.data);
+                localStorage.setItem('auth', JSON.stringify(auth));
+                axios.defaults.headers.Authorization = `Token ${auth.token}`;
 
-                const { email, name, lastname } = response.data;
+                resolve(auth);
+
+                const { email, name, lastname } = auth;
 
                 dispatch(Actions.setUserData({
                     data: { email, name, lastname, displayName: `${name} ${lastname}` }
@@ -31,18 +35,4 @@ export function activate(params)
                 reject();
             });
         });
-
-    // const request = axios.get(`/api/rfq/auth/${params}`);
-
-    // return (dispatch) =>
-    //     new Promise((resolve, reject) => {
-    //         request.then((response) => {
-    //             localStorage.setItem('auth', JSON.stringify(response.data.auth));
-    //             axios.defaults.headers.Authorization = `Token ${response.data.auth.token}`;
-    //             resolve(response.data);
-    //         })
-    //         .catch(() => {
-    //             reject();
-    //         });
-    //     });
 }
