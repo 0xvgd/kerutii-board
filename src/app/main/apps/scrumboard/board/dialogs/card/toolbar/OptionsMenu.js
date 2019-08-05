1ce63bbbad27828bac47ab827aec0041c29d57a5
voyager-gold <eugenemathew93@gmail.com>
2019-08-05 11:29:24 -0700

    Delete card confirmation dialog

--- src/app/main/apps/scrumboard/board/dialogs/card/toolbar/OptionsMenu.js
+++ src/app/main/apps/scrumboard/board/dialogs/card/toolbar/OptionsMenu.js
@@ -1,10 +1,17 @@
 import React, {useState} from 'react';
 import {Icon, IconButton, MenuItem} from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import ToolbarMenu from './ToolbarMenu';
 
 function OptionsMenu(props)
 {
     const [anchorEl, setAnchorEl] = useState(null);
+    const [confirmDelete, setConfirmDelete] = useState(false);
 
     function handleMenuOpen(event)
     {
@@ -19,7 +26,18 @@ function OptionsMenu(props)
     function handleRemoveCard()
     {
         handleMenuClose();
+        setConfirmDelete(true);
+    }
+
+    function handleDeleteClick()
+    {
+        setConfirmDelete(false);
         props.onRemoveCard();
+    }
+
+    function handleClose()
+    {
+        setConfirmDelete(false);
     }
 
     return (
@@ -34,6 +52,28 @@ function OptionsMenu(props)
                     </MenuItem>
                 </ToolbarMenu>
             )}
+
+            <Dialog
+                open={confirmDelete}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">Delete Confirmation</DialogTitle>
+                <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                    Are you going to delete this card?
+                </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                <Button onClick={handleDeleteClick} color="primary">
+                    Yes
+                </Button>
+                <Button onClick={handleClose} color="primary" autoFocus>
+                    No
+                </Button>
+                </DialogActions>
+            </Dialog>
         </div>
     );
 }
